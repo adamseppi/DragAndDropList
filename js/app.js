ebfdf642813fde38ea414dc54e7de365245ef481
WMP\aseppi <aseppi@westmonroepartners.com>
2018-03-08 15:48:38 -0600

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+(function () {
+  'use strict';
+
+  angular.module('demoApp', ['ui.tree', 'ngRoute', 'ui.bootstrap'])
+  .controller('TableExampleCtrl', ['$scope', function ($scope) {
+    $scope.remove = function (scope) {
+      scope.remove();
+    };
+
+    $scope.newItem = function () {
+      var nodeData = $scope.data[$scope.data.length - 1];
+      $scope.data.push({
+        id: $scope.data.length + 1,
+        title: 'node ' + ($scope.data.length + 1)
+      });
+    };
+
+    $scope.data = [{
+      'id': 1,
+      'title': 'node 1'
+    }, {
+      'id': 2,
+      'title': 'node 2'
+    }, {
+      'id': 3,
+      'title': 'node 3'
+    }];
+  }]);
+})();
